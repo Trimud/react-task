93bc1816c1631dce87c65ab20ea2bdf8e33eb447
Yuriy Boev <yuriy.boev@blue-leaf.co.uk>
2021-08-24 18:09:42 +0300

    Updated task description

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ function App() {
         <li>
           Use the New counter form to add multiple counters with + and -
           buttons. <br />
+          Each new counter will start from 0<br />
           They will increase/decrease the counter.
         </li>
         <li>Calculate the total number of counters</li>
       </ul>
       <h1>Counter</h1>
       <h3>Total: 12</h3>
-      {/* Add new counters here */}
+      {/* Start example here */}
       <p>
         Cats: 7 <button>+</button>&nbsp;
         <button>-</button>
@@ -25,6 +26,7 @@ function App() {
         Dogs: 5 <button>+</button>&nbsp;
         <button>-</button>
       </p>
+      {/* End example here */}
       <p>
         New counter: <input type="text" /> <button type="button">Add</button>
       </p>
